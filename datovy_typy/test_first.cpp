f4432b949c6d97661de28177801036a2d1a90955
Kristina Sinkovskaya <sinkokri@fit.cvut.cz>
2022-04-25 18:17:25 +0200

    base class change

--- datovy_typy/test_first.cpp
+++ datovy_typy/test_first.cpp
@@ -27,14 +27,23 @@ using namespace std;
 class Base
 {
 public:
-    Base                         ( const string & type ) : m_Type ( type ) { };
+                     Base        ( void ) = default;
+                     Base        ( const string & type ) : m_Type ( type ) { };
+                     Base        ( const Base & ) = default;
     virtual          ~Base       ( void ) = default;
+    virtual Base &   operator =  ( Base other ) { m_Size = other . m_Size;
+                                                  m_Type.swap( other . m_Type );
+                                                  return ( * this ); }
+
     virtual size_t   getSize     ( void )               const { return m_Size; }
-    virtual string   type        ( void )               const { return m_Type; }
-    virtual bool     operator == ( const Base & other ) const { return other . type()  == ( * this ) . type(); }
-    virtual bool     operator != ( const Base & other ) const { return other . type() != ( * this ) . type(); }
-    virtual void     print       ( ostream & os )       const { os << type(); }
-    virtual Base *   clone       () const = 0;
+    virtual string   getType     ( void )               const { return m_Type; }
+
+    virtual bool     operator == ( const Base & other ) const { return other.getType() == (*this).getType(); }
+    virtual bool     operator != ( const Base & other ) const { return other.getType() != (*this).getType(); }
+
+    virtual void     print       ( ostream & os )       const { os << getType(); }
+    virtual Base *   clone       ( void ) const { return new Base ( * this ); }
+
     friend ostream & operator << ( ostream & os,
                                    const Base & x );
 
